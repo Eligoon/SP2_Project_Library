528ad5bf4d256785372262e0d0bbe7d70811e867
Eligoon <derpepind117@live.dk>
2025-10-23 15:29:15 +0200

    right math

--- src/Net_Title.java
+++ src/Net_Title.java
@@ -19,6 +19,42 @@ public abstract class Net_Title extends Title
 
     private int getUseFactor()
     {
-        return 205;
+        if (use >= 0 && use <= 25)
+        {
+            return 0;
+        }
+        else if (use >= 26 && use <= 75)
+        {
+            return 50;
+        }
+        else if (use >= 76 && use <= 150)
+        {
+            return 100;
+        }
+        else if (use >= 151 && use <= 400)
+        {
+            return 150;
+        }
+        else if (use >= 401 && use <= 1000)
+        {
+            return 200;
+        }
+        else if (use >= 1001 && use <= 2000)
+        {
+            return 250;
+        }
+        else if (use >= 2001 && use <= 5000)
+        {
+            return 500;
+        }
+        else if (use >= 5001 && use <= 10000)
+        {
+            return 750;
+        }
+        else // use > 10000
+        {
+            return 1000;
+        }
     }
 }
+
